acb94d69e581b9711e29a86e9d821980ae2c5c43
Iakov Davydov <iakov.davydov@roche.com>
2023-11-20 14:55:13 +0100

    fit syntax highlighting for nvim-r

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,5 +5,10 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "lua",
     })
+    opts.highlight.disable = function(lang, bufnr)
+      local bufFiletype = vim.fn.getbufvar(bufnr, "&filetype")
+      if lang == "markdown" and bufFiletype == "rmd" then return true end
+      if vim.api.nvim_buf_line_count(bufnr) > 10000 then return true end
+    end
   end,
 }
