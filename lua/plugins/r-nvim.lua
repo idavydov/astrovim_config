f8424cd04ebd44c90b6a6f386d0b428f5198ec66
Iakov Davydov <671660+idavydov@users.noreply.github.com>
2024-08-29 11:50:57 +0000

    r auto start

--- lua/plugins/r-nvim.lua
+++ lua/plugins/r-nvim.lua
@@ -30,13 +30,7 @@ return {
           "RPackages",
         },
       }
-      -- Check if the environment variable "R_AUTO_START" exists.
-      -- If using fish shell, you could put in your config.fish:
-      -- alias r "R_AUTO_START=true nvim"
-      if vim.env.R_AUTO_START == "true" then
-        opts.auto_start = 1
-        opts.objbr_auto_start = true
-      end
+      if vim.env.R_AUTO_START == "1" then opts.auto_start = "on startup" end
       require("r").setup(opts)
     end,
     keys = {
