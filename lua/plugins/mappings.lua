ce324465ddc2e80904ca602a8149fe7f501aea02
Iakov Davydov <671660+idavydov@users.noreply.github.com>
2024-11-06 09:48:51 +0000

    scroll other

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -9,6 +9,14 @@ return {
           -- second key is the lefthand side of the map
           -- mappings seen under group name "Buffer"
           ["<Leader>vc"] = { "<cmd>w !diff % -<cr>", desc = "view buffer changes" },
+
+          -- scroll other window with M-j/k
+          ["<M-j>"] = { "<c-w>w<c-e><c-w>w", desc = "scroll down other window" },
+          ["<M-k>"] = { "<c-w>w<c-y><c-w>w", desc = "scroll up other window" },
+
+          -- scroll other window with M-u/d
+          ["<M-u>"] = { "<c-w>w<c-u><c-w>w", desc = "scroll up other window" },
+          ["<M-d>"] = { "<c-w>w<c-d><c-w>w", desc = "scroll down other window" },
         },
       },
     },
