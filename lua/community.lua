94530ccb3297b8e3d1c4371aba80c89b2ef8c417
Iakov Davydov <671660+idavydov@users.noreply.github.com>
2024-12-10 12:27:36 +0000

    use cmp for copilo

--- lua/community.lua
+++ lua/community.lua
@@ -6,8 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
 
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-cmp" },
 }
