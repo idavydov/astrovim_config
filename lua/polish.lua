95025ad56cb13ced5be65246ce5ccc484b0c4160
Iakov Davydov <iakov.davydov@roche.com>
2025-03-07 11:26:15 +0100

    fix rmd indentation issues

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,18 +1,12 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "rmd", "quarto" },
+  callback = function()
+    -- Remove the colon trigger so that typing ":" won't cause reindentation
+    vim.opt_local.indentkeys:remove "<:>"
+    vim.opt_local.indentkeys:append ")"
+  end,
+})
